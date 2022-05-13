225ccf60215f60e3a5fbe2c416a7a71ddda79efa
choysum22 <1240851@gmail.com>
2022-05-13 12:41:53 +0800

    fixed /time timezone

--- commands/time.js
+++ commands/time.js
@@ -1,11 +1,13 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+var moment = require('moment-timezone');
 
 function getTime() {
     // get current time and add 30 minutes
     const now = new Date();
     const later = new Date(now.getTime() + 30 * 60000);
+	const tonytime = moment.tz(later, "Asia/Hong_Kong").format("HH:mm");
     // return time in format: 'hh:mm'
-    return later.toLocaleTimeString('en-US', { hour12: false, hour: 'numeric', minute: 'numeric' });
+    return tonytime;
 
 }
 
@@ -14,6 +16,6 @@ module.exports = {
 		.setName('time')
 		.setDescription('get current time in Tony timezone (GMT+8.5)'),
 	async execute(interaction) {
-		await interaction.reply(getTime() + ' (GMT+8.5)');
+		await interaction.reply('It is now '+ getTime() + ' (GMT+8.5) in Tony timezone');
 	},
 };
